981c2b63c86c18fd0692137514582bb10edda4cc
RareDango <jrwhughes@gmail.com>
2026-02-05 13:32:01 -0800

    Updated date

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const gameVersion = "0.4.3";
-export const updatedDate = "Wed, Feb 4, 2026";
+export const updatedDate = "Thu, Feb 5, 2026";
 
 export const MAX_UNITS = 32; // hangs if players set above 21. no idea why -> It was because that's how many quotes there were to pick from
 export const DEFAULT_NUM_PLAYERS = 4;
