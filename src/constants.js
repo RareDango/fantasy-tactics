7956b2aa3fc126793f36ed91dded5c64809d3402
RareDango <jrwhughes@gmail.com>
2026-02-08 22:46:51 -0800

    One more minor adjustment to unit numbers

--- src/constants.js
+++ src/constants.js
@@ -2,8 +2,8 @@ export const GAME_VERSION = "0.5.7";
 export const DATE_UPDATED = "Sun, Feb 8, 2026";
 
 export const MAX_UNITS = 64;
-export const DEFAULT_NUM_PLAYERS = 4;
-export const DEFAULT_NUM_ENEMIES = 8;
+export const DEFAULT_NUM_PLAYERS = 5;
+export const DEFAULT_NUM_ENEMIES = 9;
 export const MAX_FIREWORKS = 8;
 
 export const TILE_SIZE   = 64;
